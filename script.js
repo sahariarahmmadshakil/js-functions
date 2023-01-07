9155bd70b50fcea7e90ce9ba9ea531531cd9e70c
sahariarahmmad <info.sashakil@gmail.com>
2023-01-08 00:15:13 +0600

    added finding property from object concept

--- script.js
+++ script.js
@@ -14,6 +14,28 @@
 // products.price=5555;
 // console.log(products.price);
 
+// const shopping={
+//     books:5,
+//     papers:16,
+//     penColor: 'red',
+//     booksName:'Stastics',
+//     mouse:2,
+//     mouseBrand:'logitech'
+// }
+// const names = Object.values(shopping);
+// const keys = Object.keys(shopping);
+// console.log(names);
+// console.log(keys);
+// first way to get propertys 
+// const propartyFway=shopping.penColor;
+// console.log(propartyFway);
+// // second way 
+// const propertySway=Object.values(shopping);
+// const propertySwayk=Object.keys(shopping);
+// console.log(propertySway);
+// console.log(propertySwayk);
+
+// problem 1: find the property of the objects
 const shopping={
     books:5,
     papers:16,
@@ -22,5 +44,8 @@ const shopping={
     mouse:2,
     mouseBrand:'logitech'
 }
-const mName=shopping.mouseBrand;
-console.log(mName);
+// finding specific properties from bject 
+const penColor1 = shopping.penColor;
+console.log(penColor1);
+const penColor2 = shopping['penColor'];
+console.log(penColor2);
