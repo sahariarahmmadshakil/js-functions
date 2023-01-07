7ab083dd027f186274ea21dfe08eac423523a3b8
sahariarahmmad <info.sashakil@gmail.com>
2023-01-07 23:58:54 +0600

    added object

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-function evenOdd(number){
-   if(number%2==0){
-    console.log(number, 'is Even Number');
-   }else{
-    console.log(number, "is Odd Number");
-   }
+// function evenOdd(number){
+//    if(number%2==0){
+//     console.log(number, 'is Even Number');
+//    }else{
+//     console.log(number, "is Odd Number");
+//    }
+// }
+// evenOdd(15);
+
+// const products={
+//     product1: 'mobile', price: 12000,
+//     product2: 'laptop',color: 'pink'
+// }
+// products.price=5555;
+// console.log(products.price);
+
+const shopping={
+    books:5,
+    papers:16,
+    penColor: 'red',
+    booksName:'Stastics',
+    mouse:2,
+    mouseBrand:'logitech'
 }
-evenOdd(15);
+const mName=shopping.mouseBrand;
+console.log(mName);
